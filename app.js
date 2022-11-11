10a307481d31e05bd123a02c37356b2a41a55b58
chengzhangao <gchengzhan@gmail.com>
2022-11-10 16:21:23 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const path = require('path')
+const Campground = require('./models/campground')
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+}) ;// databaseName:yelp-camp
+
+//connect to mongoose
+const db= mongoose.connection;
+db.on("error",console.error.bind(console,"connection error:"));
+db.once("open",()=>{
+    console.log("Database connected");
+});
+
+const app = express();
+app.set('view engine','ejs')
+app.set('views',path.join(__dirname,'views'))
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+
+
+
+app.get('/makecampground', async (req,res)=>{
+    const camp = new Campground({title:'My Backyard',
+                                price: '2.2',
+                                description: 'cheep',
+                                location: 'California'});
+    await camp.save()  
+    res.send(camp);
+})
+app.listen(3000,()=>{
+    console.log('Serving on Port 3000')
+})
