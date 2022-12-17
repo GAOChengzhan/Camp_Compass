341af025fa5d4014025ee67bcf77aed98a812189
chengzhangao <gchengzhan@gmail.com>
2022-12-16 18:24:32 -0800

    register/login

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,34 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 const User = require('../models/user');
+const catchAsync = require('../utils/catchAsync');
 
 router.get('/register',(req,res)=>{
     res.render('users/register');
 })
+router.post('/register',catchAsync(async(req,res)=>{
+    try{
+        const {username,email,password} = req.body;
+        const user = new User({email,username});
+        const registeredUser = await User.register(user,password);
+        req.flash('success','Registered Successfully! Welcome to Camp, '+username+'!');
+        res.redirect('/campgrounds');
+    }
+    catch(e){
+        req.flash('error',e.message);
+        res.redirect('/register');
+    }
+}));
 
+router.get('/login',(req,res)=>{
+    res.render('users/login');
+})
+router.post('/login',
+            passport.authenticate('local',
+                                {failureFlash:true,failureRedirect:'/login'}),
+            (req,res)=>{
+    req.flash('success','Welcome Back, '+req.body.username+'!');
+    res.redirect('/campgrounds');
+})
 module.exports = router;
